f5e6a04eb460ba5d0cba850d8c458e759ceee527
Mohammad Dahamshi <mohammad.dahamshi@gmail.com>
2025-08-17 19:03:03 +0300

    adds storage

---
+++ server/src/services/cloudinary.js
@@ -0,0 +1,24 @@
+// src/services/cloudinary.js
+import { v2 as cloudinary } from 'cloudinary'
+
+if (process.env.STORAGE_PROVIDER === 'cloudinary') {
+  cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+    secure: true,
+  })
+}
+
+export async function uploadToCloudinary(buffer, mimeType, folder, filename) {
+  return new Promise((resolve, reject) => {
+    const uploadStream = cloudinary.uploader.upload_stream(
+      { folder, resource_type: 'auto', filename_override: filename, use_filename: true, unique_filename: false },
+      (err, result) => {
+        if (err) return reject(err)
+        resolve(result.secure_url)
+      }
+    )
+    uploadStream.end(buffer)
+  })
+}
