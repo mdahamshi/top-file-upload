e61047d640bfa9a6bf94f424838d99e61fc16843
Mohammad Dahamshi <mohammad.dahamshi@gmail.com>
2025-08-26 16:32:34 +0300

    update seed

--- server/src/db/init/seed.js
+++ server/src/db/init/seed.js
@@ -1,38 +1,42 @@
-// prisma/seed.ts
+// src/db/init/seed.js
 import { prisma } from '../../utils/prisma.js';
 
+const users = [
+  {
+    fname: 'Mohammad',
+    lname: 'Dahamshi',
+    email: 'mohammad.dahamshi@gmail.com',
+    passwordHash:
+      '$2b$10$x6gir2ZBJ399bARIxkow7uhrOw8fqPojsMeiAc1xMCRRL6CCqwDGS',
+  },
+  {
+    fname: 'Sarah',
+    lname: 'Dahamshi',
+    email: 'sarah.dahamshi@gmail.com',
+    passwordHash:
+      '$2b$10$cnAf72pKB2/w.d134QJu1O51s.PSfw0dhwXwWTKkq92XPbaTIUjUC',
+  },
+  {
+    fname: 'Salmah',
+    lname: 'Dahamshi',
+    email: 'salmah.dahamshi.22@gmail.com',
+    passwordHash:
+      '$2b$10$cnAf72pKB2/w.d134QJu1O51s.PSfw0dhwXwWTKkq92XPbaTIUjUC',
+  },
+];
+
 async function main() {
-  const res = await prisma.user.createMany({
-    data: [
-      {
-        fname: 'Mohammad',
-        lname: 'Dahamshi',
-        email: 'mohammad.dahamshi@example.com',
-        passwordHash:
-          '$2b$10$x6gir2ZBJ399bARIxkow7uhrOw8fqPojsMeiAc1xMCRRL6CCqwDGS',
-      },
-      {
-        fname: 'Sarah',
-        lname: 'Dahamshi',
-        email: 'sarah.dahamshi@example.com',
-        passwordHash:
-          '$2b$10$cnAf72pKB2/w.d134QJu1O51s.PSfw0dhwXwWTKkq92XPbaTIUjUC',
-      },
-      {
-        fname: 'Salmah',
-        lname: 'Dahamshi',
-        email: 'salmah.dahamshi.22@gmail.com',
-        passwordHash:
-          '$2b$10$cnAf72pKB2/w.d134QJu1O51s.PSfw0dhwXwWTKkq92XPbaTIUjUC',
-      },
-    ],
-    skipDuplicates: true,
-  });
+  for (const u of users) {
+    const user = await prisma.user.create({ data: u });
+    console.log(`✅ Created user ${user.fname} ${user.lname} with root folder`);
+  }
+
+
 }
 
 main()
   .catch((e) => {
-    console.error(e);
+    console.error('❌ Error seeding users:', e);
     process.exit(1);
   })
   .finally(async () => {
